71c06aeb8007d951fb02b29fb258985ca26223db
Hiago Isoppo <hiagoisoppotrajano@gmail.com>
2023-10-31 21:52:14 -0300

    Implementing BlogPost model and migrations

---
+++ src/migrations/20231101004939-create-postscategories.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
---
+++ src/models/BlogPost.js
@@ -0,0 +1,24 @@
+const BlogPostSchema = (sequelize, DataTypes) => {
+  const BlogPostTable = sequelize.define('BlogPost', {
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+    userId: DataTypes.INTEGER,
+  }, {
+    tableName: 'blogPosts',
+    underscored: true,
+    timestamps: false,
+  });
+
+  BlogPostTable.associate = (models) => {
+    BlogPostTable.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  };
+
+  return BlogPostTable;
+}
+
+module.exports = BlogPostSchema;
