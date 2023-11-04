8f143b2edf055b6db48d4fd4ae0640e5f0f1f41b
Hiago Isoppo <hiagoisoppotrajano@gmail.com>
2023-11-04 00:00:28 -0300

    Updating README and env.example file

--- tests/unit/example.test.js
+++
@@ -1,7 +0,0 @@
-const { expect } = require('chai');
-
-describe('Hello', function () {
-  it('should return hello world', function () {
-    expect('Hello world!').to.equal('Hello world!');
-  });
-});
